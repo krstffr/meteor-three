59ec3afb368079074db70c0ffb243eb3740af51a
Alexander Ottenhoff <alexander.ottenhoff@gmail.com>
2014-07-25 14:19:00 +0200

    Exorting THREE everywhere

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
 Package.on_use(function(api) {
 	api.add_files(["lib/three.js"], "client");
 
-	api.export("THREE", "client");
+	api.export("THREE");
 
 	api.add_files([
 		// Loaders
