2ee35ab802789c39ae2eed517381770900e287b3
Kristoffer Klintberg <krstffr@gmail.com>
2014-10-29 13:26:51 +0100

    Removed all textra stuff, added typedarray polyfill

--- package.js
+++ package.js
@@ -1,92 +1,16 @@
 Package.describe({
-	summary: "Three.js core library + several useful extensions from the examples folder.",
-	version: "0.68.2",
-	name: "limemakers:three",
-	git: "https://github.com/limemakers/meteor-three.git",
+	summary: "Three.js core library + IE8 Polyfill.",
+	version: "0.68.3",
+	name: "krstffr:three",
+	git: "https://github.com/krstffr/meteor-three.git",
 });
 
+Npm.depends({ typedarray: "0.0.6" });
+
 Package.on_use(function(api) {
-	api.versionsFrom('0.9.0');
+
+	api.add_files(["lib/three.js"], "client");
+
 	api.export("THREE", "client");
 
-	api.add_files([
-		// Core
-		"lib/three.js",
-
-		// Loaders
-		"extras/loaders/OBJMTLLoader.js",
-		"extras/loaders/MTLLoader.js",
-		"extras/loaders/OBJLoader.js",
-		"extras/loaders/DDSLoader.js",
-		"extras/loaders/ctm/CTMLoader.js",
-
-		// There doesn't appear to be a good way to package worker scripts in Meteor ATM,
-		// so you can either move these files into public yourself or just use the
-		// single threaded CTM Loader:
-		// "extras/loaders/ctm/CTMWorker.js",
-		// "extras/loaders/ctm/ctm.js",
-		// "extras/loaders/ctm/lzma.js",
-
-		// Exporters
-		"extras/exporters/STLBinaryExporter.js",
-		"extras/exporters/STLExporter.js",
-		"extras/exporters/OBJExporter.js",
-		"extras/exporters/MaterialExporter.js",
-
-		// Controls
-		"extras/controls/TrackballControls.js",
-		"extras/controls/OrbitControls.js",
-		// "extras/controls/FlyControls.js",
-
-		// PostProcessing
-		"extras/postprocessing/BloomPass.js",
-		"extras/postprocessing/BokehPass.js",
-		"extras/postprocessing/DotScreenPass.js",
-		"extras/postprocessing/EffectComposer.js",
-		"extras/postprocessing/FilmPass.js",
-		"extras/postprocessing/GlitchPass.js",
-		"extras/postprocessing/MaskPass.js",
-		"extras/postprocessing/RenderPass.js",
-		"extras/postprocessing/SavePass.js",
-		"extras/postprocessing/ShaderPass.js",
-		"extras/postprocessing/TexturePass.js",
-
-		// Shaders
-		"extras/shaders/BasicShader.js",
-		"extras/shaders/BleachBypassShader.js",
-		"extras/shaders/BlendShader.js",
-		"extras/shaders/BokehShader.js",
-		"extras/shaders/BokehShader2.js",
-		"extras/shaders/BrightnessContrastShader.js",
-		"extras/shaders/ColorCorrectionShader.js",
-		"extras/shaders/ColorifyShader.js",
-		"extras/shaders/ConvolutionShader.js",
-		"extras/shaders/CopyShader.js",
-		"extras/shaders/DigitalGlitch.js",
-		"extras/shaders/DOFMipMapShader.js",
-		"extras/shaders/DotScreenShader.js",
-		"extras/shaders/EdgeShader.js",
-		"extras/shaders/EdgeShader2.js",
-		"extras/shaders/FilmShader.js",
-		"extras/shaders/FocusShader.js",
-		"extras/shaders/FresnelShader.js",
-		"extras/shaders/FXAAShader.js",
-		"extras/shaders/HorizontalBlurShader.js",
-		"extras/shaders/HorizontalTiltShiftShader.js",
-		"extras/shaders/HueSaturationShader.js",
-		"extras/shaders/KaleidoShader.js",
-		"extras/shaders/LuminosityShader.js",
-		"extras/shaders/MirrorShader.js",
-		"extras/shaders/NormalMapShader.js",
-		"extras/shaders/OceanShaders.js",
-		"extras/shaders/RGBShiftShader.js",
-		"extras/shaders/SepiaShader.js",
-		"extras/shaders/SSAOShader.js",
-		"extras/shaders/TechnicolorShader.js",
-		"extras/shaders/TriangleBlurShader.js",
-		"extras/shaders/UnpackDepthRGBAShader.js",
-		"extras/shaders/VerticalBlurShader.js",
-		"extras/shaders/VerticalTiltShiftShader.js",
-		"extras/shaders/VignetteShader.js"
-	], "client");
 });
